40aed2726dff6ec3193f6fe139af9b7eafb8fca9
Levi Jasper Alicaya <l.alicaya.557990@umindanao.edu.ph>
2026-05-20 21:24:23 +0800

    Use Vite for custom CSS on all pages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
     
     <!-- Custom CSS -->
-    <link href="http://student-enrollment-info-system-2.onrender.com/css/style.css" rel="stylesheet">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     
     @stack('styles')
 </head>
